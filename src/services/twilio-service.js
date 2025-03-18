661b53a9bd77f7b875971700e9ff14dc158edac6
francproductmanager <francproductmanager@gmail.com>
2025-03-18 19:19:03 +0000

    Update twilio-service.js

--- src/services/twilio-service.js
+++ src/services/twilio-service.js
@@ -1,24 +1,18 @@
-// /services/twilio-service.js
+// src/services/twilio-service.js
 const Twilio = require('twilio');
-const { messageTracker, isTestMode } = require('../utils/testing-utils');
 const { logDetails } = require('../utils/logging-utils');
 
 // Twilio client wrapper
 class TwilioClientWrapper {
   constructor(req) {
     this.req = req;
-    this.testMode = isTestMode(req);
+    this.testMode = req && req.isTestMode;
     
     // Initialize real Twilio client if credentials exist
     if (process.env.ACCOUNT_SID && process.env.AUTH_TOKEN) {
       this.realClient = new Twilio(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
     } else {
       this.realClient = null;
-    }
-    
-    // Reset message tracker in test mode
-    if (this.testMode) {
-      messageTracker.reset();
     }
   }
   
@@ -31,12 +25,17 @@ class TwilioClientWrapper {
     if (this.testMode) {
       // Log the message but don't actually send it
       logDetails(`[TEST MODE] Would send message to ${options.to}:`, options.body);
-      messageTracker.addMessage({
-        to: options.to,
-        from: options.from,
-        body: options.body,
-        timestamp: new Date().toISOString()
-      });
+      
+      // Add to test results if available
+      if (this.req && this.req.testResults) {
+        this.req.testResults.messages.push({
+          to: options.to,
+          from: options.from,
+          body: options.body,
+          timestamp: new Date().toISOString()
+        });
+      }
+      
       return {
         sid: 'TEST-SID-' + Math.random().toString(36).substring(2, 15),
         status: 'test-queued',
@@ -52,19 +51,16 @@ class TwilioClientWrapper {
   
   // Used to send TwiML responses
   generateXMLResponse(xml) {
-    if (this.testMode) {
-      // Store XML in tracker but don't actually return it to the client
-      messageTracker.setXMLResponse(xml);
+    if (this.testMode && this.req && this.req.testResults) {
+      // Store XML in test results
+      this.req.testResults.xmlResponse = xml;
     }
     return xml;
   }
   
   // Get test results
   getTestResults() {
-    if (this.testMode) {
-      return messageTracker.getResults();
-    }
-    return null;
+    return (this.req && this.req.testResults) || null;
   }
 }
 
