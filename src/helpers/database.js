6322acfd59554a1f98a67764d9d28b39b1c85b4b
francproductmanager <francproductmanager@gmail.com>
2026-01-02 12:04:01 +0000

    Harden database SSL verification defaults

--- src/helpers/database.js
+++ src/helpers/database.js
@@ -3,12 +3,13 @@ const { Pool } = require('pg');
 const { detectCountryCode } = require('./localization');
 const { logDetails } = require('../utils/logging-utils');
 
+const sslEnabled = process.env.DATABASE_SSL !== 'false';
+const rejectUnauthorized = process.env.DATABASE_SSL_REJECT_UNAUTHORIZED !== 'false';
+
 // Create a connection pool
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { 
-    rejectUnauthorized: false 
-  }
+  ssl: sslEnabled ? { rejectUnauthorized } : false
 });
 
 // Check if a user exists by phone number, create if not
